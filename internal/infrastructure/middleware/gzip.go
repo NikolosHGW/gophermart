daf89f5d36e88283c341abf65c196faeafc79d65
Nikita Bakarev <nic231731@yandex.ru>
2024-05-25 12:19:29 +0300

    fix: change gzip

--- internal/infrastructure/middleware/gzip.go
+++ internal/infrastructure/middleware/gzip.go
@@ -42,9 +42,7 @@ func (c *compressWriter) Write(p []byte) (int, error) {
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
-	if statusCode < statusMultipleChoices {
-		c.w.Header().Set("Content-Encoding", "gzip")
-	}
+	c.w.Header().Set("Content-Encoding", "gzip")
 	c.w.WriteHeader(statusCode)
 }
 
