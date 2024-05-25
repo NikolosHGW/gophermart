53f7b03d147fd11911a148fa334181f66842527b
Nikita Bakarev <nic231731@yandex.ru>
2024-05-25 13:57:30 +0300

    fix: add accrual for entity order

--- internal/domain/entity/order.go
+++ internal/domain/entity/order.go
@@ -1,7 +1,8 @@
 package entity
 
 type Order struct {
-	Status     string `json:"status" db:"status"`
-	UploadedAt string `json:"uploaded_at" db:"uploaded_at"`
-	Number     string `json:"number" db:"number"`
+	Status     string  `json:"status" db:"status"`
+	UploadedAt string  `json:"uploaded_at" db:"uploaded_at"`
+	Number     string  `json:"number" db:"number"`
+	Accrual    float64 `json:"accrual" db:"accrual"`
 }
